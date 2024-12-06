a490d66026c9713142470c266414a7e809717b3a
lanrengm <lanrengm@gmail.com>
2024-12-06 14:39:19 +0800

    使用 css

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
 export function Clock({time}: {time: string}) {
-  useState()
-  return <div>{time}</div>;
+  return <div className="tl-clock">{time}</div>;
 }
