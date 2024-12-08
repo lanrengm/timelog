f78475a97e9e38199bc6a1e31050d1be604273a9
lanrengm <lanrengm@gmail.com>
2024-12-08 11:03:52 +0800

    移除 React 框架

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -15,9 +15,6 @@ import {
 } from "obsidian";
 import { observable, action, autorun, flow, runInAction, computed } from "mobx";
 import * as d3 from "d3";
-import React, { useState } from "react";
-import { createRoot } from "react-dom/client";
-import type * as CSS from "csstype";
 
 import { PLUGIN_VIEW_TYPE, PLUGIN_FILE_EXT, PLUGIN_ICON, PLUGIN_NAME, Plan, Timelog, DEFAULT_FILE_DATA, Record, TIME_FMT, TIME_FMT_ID } from "../settings";
 import { timeSub } from "../utils";
@@ -83,7 +80,7 @@ export class TimelogView extends FileView {
   constructor(leaf: WorkspaceLeaf, plugin: TimelogPlugin) {
     super(leaf);
     this.plugin = plugin;
-    this.contentEl.setCssStyles({padding: 'var(--file-margins)'});
+    this.contentEl.setCssStyles({ padding: "var(--file-margins)" });
     const ctlEl = this.contentEl.createDiv({ cls: "timelog-ctl" });
     // 不同的主题渲染表格采用的选择器不同
     // 为了兼容多种主题，添加了 markdown-rendered markdown-preview-view
@@ -106,26 +103,25 @@ export class TimelogView extends FileView {
     );
 
     // 测试块
-    const root = createRoot(this.ctlEl.createDiv());
-    autorun(() => root.render(<Ctl timeStr={this.timer.value} />));
-
+    Clock(this.ctlEl, (render) => {
+      autorun(() => {
+        const dateStr = this.timer.value.replace(/\s[0-9:]*$/, "");
+        const timeStr = this.timer.value.replace(/^[0-9\-]*\s/, "");
+        render(dateStr, timeStr);
+      });
+    });
     this.doing();
-
-    const root2 = createRoot(this.ctxEl.createDiv());
-    autorun(() => {
-      const lastRecord = this.timelog.records.slice(-5).reverse();
-      // 不加下一行 MobX 监听不到 stop 的修改。
-      this.timelog.records.last()?.stop;
-      root2.render(<RecordsTable records={lastRecord} plans={this.timelog.plans} />);
+    RecordsTable(this.ctxEl, (render) => {
+      autorun(()=>{
+        const lastRecord = this.timelog.records.slice(-5).reverse();
+        render(this.timelog.plans, lastRecord);
+      });
     });
-
-    const root3 = createRoot(this.ctxEl.createDiv());
-    autorun(() => {
-      this.timelog.plans.length;
-      root3.render(<PlansTable plans={this.timelog.plans} />);
+    PlansTable(this.ctxEl, (render)=>{
+      autorun(()=>{
+        render(this.timelog.plans);
+      });
     });
-
-    // this.showRecordsChart();
   }
 
   async onClose(): Promise<void> {
@@ -224,7 +220,7 @@ export class TimelogView extends FileView {
   /**
    * 计时器，用来刷新时间
    */
-  timer = observable({ value: "" });
+  timer = observable({ value: "0000-00-00 00:00:00" });
 
   /**
    * 心跳，一个时钟，用来刷新时间
@@ -297,10 +293,6 @@ export class TimelogView extends FileView {
         await this.saveTimelog();
       }
     }
-  }
-
-  XX() {
-    return <div>XX</div>;
   }
 }
 
@@ -407,97 +399,100 @@ class PlanModal extends Modal {
 }
 
 /**
- * 整个视图内容，挂载到 obsidian view 的 content 上
- */
-function ViewX() {
-  return (
-    <>
-      <Ctl timeStr="timeStr"/>
-    </>
-  );
-}
-
-/**
- * 视图控制区，控制面板
- */
-function Ctl({ timeStr }: { timeStr: string }) {
-  return (
-    <div className="timelog-ctl">
-      <Clock timeStr={timeStr} />
-    </div>
-  );
-}
-
-/**
- * 视图内容区
- */
-function Ctx({plans}: {plans: Plan[]}) {
-  return (<>
-    <PlansTable plans={plans}/>
-  </>);
-}
-
-/**
  * 刷新计划视图（表格视图）
  */
-function PlansTable({ plans }: { plans: Plan[] }) {
-  return (
-    <>
-      <h2>计划</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>名称</th>
-          </tr>
-        </thead>
-        <tbody>
-          {plans.map(plan => (
-            <tr key={plan.id}>
-              <td>{plan.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+function PlansTable(mountedEl: HTMLElement, callRender: (render: (plans: Plan[])=>void)=>void): HTMLElement {
+  const rootEl = mountedEl.createDiv({});
+  rootEl.innerHTML = /*html*/ `
+    <h2>计划</h2>
+    <table>
+      <thead><tr><th>名称</th></tr></thead>
+      <tbody></tbody>
+    </table>
+  `;
+  /**
+   * 渲染视图
+   */
+  const bodyEl = rootEl.querySelector('table tbody');
+  callRender((plans) => {
+    bodyEl?.empty();
+    let body = '';
+    for (const plan of plans) {
+      body += /*html*/ `<tr><td>${plan.name}</td></tr>`;
+    }
+    if(bodyEl) bodyEl.innerHTML = body;
+  });
+  return rootEl;
 }
 
 /**
  * 刷新记录视图（表格形式）
  */
-function RecordsTable({ plans, records }: { plans: Plan[]; records: Record[] }) {
-  return (
-    <>
-      <h2>记录</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>计划</th>
-            <th>时长</th>
-            <th>开始时间</th>
-            <th>结束时间</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records.map(record => (
-            <tr key={record.start + record.stop}>
-              <td>{plans.find(plan => record.id === plan.id)!.name ?? ""}</td>
-              <td>{timeSub(record.start, record.stop, TIME_FMT)}</td>
-              <td>{record.start}</td>
-              <td>{record.stop}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+function RecordsTable(mountedEl: HTMLElement, callRender: (render: (plans: Plan[], records: Record[]) => void) => void): HTMLElement {
+  /**
+   * HTML
+   */
+  const rootEl = mountedEl.createDiv({});
+  rootEl.innerHTML = /*html*/ `
+    <h2>记录</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>计划</th>
+          <th>时长</th>
+          <th>开始时间</th>
+          <th>结束时间</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  `;
+  /**
+   * 视图刷新
+   */
+  const tbodyEl: HTMLTableSectionElement = rootEl.querySelector("table tbody")!;
+  callRender((plans, records) => {
+    tbodyEl.empty();
+    let body = '';
+    for (const record of records) {
+      body += /*html*/ `
+        <tr>
+          <td>${plans.find(plan => record.id === plan.id)!.name ?? ""}</td>
+          <td>${timeSub(record.start, record.stop, TIME_FMT)}</td>
+          <td>${record.start}</td>
+          <td>${record.stop}</td>
+        </tr>
+      `;
+    }
+    tbodyEl.innerHTML = body;
+  });
+  return rootEl;
 }
 
-export function Clock({ timeStr }: { timeStr: string }) {
-  const container: CSS.Properties = {
-    // padding: "2em",
-  };
-  const wrapper: CSS.Properties = {
+/**
+ * 时钟组件
+ * @param mountedEl 挂载点
+ * @param callRender 刷新视图的回调函数
+ * @returns 组件根，mounted/cptRoot
+ */
+function Clock(mountedEl: HTMLElement, callRender: (render: (dateStr: string, timeStr: string) => void)=>void ): HTMLElement {
+  /**
+   * HTML
+   */
+  const rootEl = mountedEl.createDiv({ cls: "tl-container" });
+  rootEl.innerHTML = /*html*/ `
+    <div class="tl-wrapper">
+      <div class="text">YYYY-MM-DD</div>
+      <div class="text">HH:mm:ss</div>
+    </div>
+  `;
+  /**
+   * CSS
+   */
+  rootEl?.setCssStyles({});
+  const wrapperEl: HTMLDivElement | null = rootEl.querySelector(".tl-wrapper");
+  wrapperEl?.setCssStyles({
     display: "flex",
     alignSelf: "center",
     justifyContent: "space-around",
@@ -506,19 +501,29 @@ export function Clock({ timeStr }: { timeStr: string }) {
     gap: "1em",
     border: "var(--hr-color) solid 3px",
     borderRadius: "2em",
-  };
-  const text: CSS.Properties = {
-    color: "var(--h2-color)",
-    fontSize: "var(--h2-size)",
-    fontWeight: "bold",
-    lineHeight: "var(--h2-line-height)",
-  };
-  return (
-    <div style={container}>
-      <div style={wrapper}>
-        <div style={text}>{timeStr.replace(/\s[0-9:]*$/, "")}</div>
-        <div style={text}>{timeStr.replace(/^[0-9\-]*\s/, "")}</div>
-      </div>
-    </div>
+  });
+  const textEls: NodeListOf<HTMLDivElement> = rootEl.querySelectorAll(".text");
+  textEls.forEach(textEl =>
+    textEl.setCssStyles({
+      color: "var(--h2-color)",
+      fontSize: "var(--h2-size)",
+      fontWeight: "bold",
+      lineHeight: "var(--h2-line-height)",
+    })
   );
+  /**
+   * 视图刷新
+   */
+  const dateEl = textEls[0];
+  const timeEl = textEls[1];
+  callRender((dateStr, timeStr)=>{
+    dateEl.innerText = dateStr;
+    timeEl.innerText = timeStr;
+  });
+
+  if (rootEl) {
+    return rootEl;
+  } else {
+    throw Error("组件模板错误");
+  }
 }
