30db7385bdab07b43050681a4a9f2a27651a4a59
lanrengm <lanrengm@gmail.com>
2024-12-08 18:56:13 +0800

    Merge branch 'dev'

--- src/views/view.ts
+++ src/views/view.ts
@@ -12,12 +12,10 @@ import {
   Modal,
   MomentFormatComponent,
   moment,
+  ExtraButtonComponent,
 } from "obsidian";
 import { observable, action, autorun, flow, runInAction, computed } from "mobx";
 import * as d3 from "d3";
-import React, { useState } from "react";
-import { createRoot } from "react-dom/client";
-import type * as CSS from "csstype";
 
 import { PLUGIN_VIEW_TYPE, PLUGIN_FILE_EXT, PLUGIN_ICON, PLUGIN_NAME, Plan, Timelog, DEFAULT_FILE_DATA, Record, TIME_FMT, TIME_FMT_ID } from "../settings";
 import { timeSub } from "../utils";
@@ -83,12 +81,18 @@ export class TimelogView extends FileView {
   constructor(leaf: WorkspaceLeaf, plugin: TimelogPlugin) {
     super(leaf);
     this.plugin = plugin;
-    this.contentEl.setCssStyles({padding: 'var(--file-margins)'});
+    this.contentEl.setCssStyles({ padding: "var(--file-margins)" });
     const ctlEl = this.contentEl.createDiv({ cls: "timelog-ctl" });
     // 不同的主题渲染表格采用的选择器不同
     // 为了兼容多种主题，添加了 markdown-rendered markdown-preview-view
     const ctxEl = this.contentEl.createDiv({ cls: "timelog-ctx markdown-rendered markdown-preview-view" });
     this.ctlEl = ctlEl;
+    this.ctlEl.setCssStyles({
+      display: "flex",
+      justifyContent: 'space-around',
+      flexWrap: "wrap",
+      gap: "1em"
+    })
     this.ctxEl = ctxEl;
     DEV ?? console.log(`new TimeLogView(${leaf}, ${plugin})`);
   }
@@ -106,26 +110,57 @@ export class TimelogView extends FileView {
     );
 
     // 测试块
-    const root = createRoot(this.ctlEl.createDiv());
-    autorun(() => root.render(<Ctl timeStr={this.timer.value} />));
+    const clockRender = Clock(this.ctlEl);
+    autorun(() => {
+      const t = this.isDoing.get() ? timeSub(this.timelog.records.last()!.start, this.timer.value, TIME_FMT) : this.timer.value;
+      const dateStr = t.replace(/\s[0-9:]*$/, "");
+      const timeStr = t.replace(/^[0-9\-]*\s/, "");
+      clockRender(dateStr, timeStr);
+    });
+    this.ctlEl.createDiv().setCssStyles({height: "1em"});
+    const s2 = this.ctlEl.createDiv();
+    s2.setCssStyles({
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      gap: '1em'
+    })
+    // 
+    const planSelectRender = PlanSelect(s2, (evt: Event) => {
+      runInAction(() => {
+        this.selectedPlanId.value = (evt.target as HTMLSelectElement).value;
+      });
+    });
+    autorun(() => {
+      console.log("渲染" + this.selectedPlanId.value);
 
-    this.doing();
-
-    const root2 = createRoot(this.ctxEl.createDiv());
+      const v = planSelectRender(this.timelog.plans, this.timelog.records);
+      runInAction(() => {
+        this.selectedPlanId.value = v;
+      });
+    });
+    // 开始/结束 按钮
+    const startButton = new StartButton(s2).onClick(() => {
+      if (this.isDoing.get()) {
+        this.stopPlan();
+      } else {
+        this.startPlan();
+      }
+    });
+    autorun(()=>{
+      startButton.toggleIcon(this.isDoing.get());
+    })
+    // 
+    const recordsTableRender = RecordsTable(this.ctxEl);
     autorun(() => {
       const lastRecord = this.timelog.records.slice(-5).reverse();
-      // 不加下一行 MobX 监听不到 stop 的修改。
-      this.timelog.records.last()?.stop;
-      root2.render(<RecordsTable records={lastRecord} plans={this.timelog.plans} />);
+      recordsTableRender(this.timelog.plans, lastRecord);
     });
-
-    const root3 = createRoot(this.ctxEl.createDiv());
+    const plansTableRender = PlansTable(this.ctxEl);
     autorun(() => {
-      this.timelog.plans.length;
-      root3.render(<PlansTable plans={this.timelog.plans} />);
+      plansTableRender(this.timelog.plans);
     });
-
-    // this.showRecordsChart();
   }
 
   async onClose(): Promise<void> {
@@ -142,50 +177,6 @@ export class TimelogView extends FileView {
   async onUnloadFile(file: TFile): Promise<void> {
     DEV ?? console.log(`onUnloadFile(${file.name})`);
     // 切换时光日志时触发
-  }
-
-  doing() {
-    const heading = new Setting(this.ctlEl);
-
-    autorun(() => {
-      if (this.isDoing.get()) {
-        const time = timeSub(this.timelog.records.last()!.start, this.timer.value, TIME_FMT);
-        heading.setName(time);
-      } else {
-        heading.setName(`${this.timer.value}`);
-      }
-    });
-
-    heading.addDropdown(dd => {
-      // this.timelog 更新后刷新视图
-      autorun(() => {
-        dd.selectEl.empty();
-        this.timelog.plans.forEach(plan => dd.addOption(plan.id, plan.name));
-        if (this.timelog.records.length !== 0) {
-          const lastRecordId = this.timelog.records.last()!.id;
-          dd.setValue(lastRecordId);
-        }
-        // 刷新状态
-        runInAction(() => {
-          this.selectedPlanId.value = dd.getValue();
-        });
-      });
-      dd.onChange(value => {
-        runInAction(() => {
-          this.selectedPlanId.value = value;
-        });
-      });
-    });
-
-    heading.addExtraButton(btn => {
-      autorun(() => {
-        if (this.isDoing.get()) {
-          btn.setIcon("square").onClick(() => this.stopPlan());
-        } else {
-          btn.setIcon("play").onClick(() => this.startPlan());
-        }
-      });
-    });
   }
 
   /**
@@ -224,7 +215,7 @@ export class TimelogView extends FileView {
   /**
    * 计时器，用来刷新时间
    */
-  timer = observable({ value: "" });
+  timer = observable({ value: "0000-00-00 00:00:00" });
 
   /**
    * 心跳，一个时钟，用来刷新时间
@@ -297,10 +288,6 @@ export class TimelogView extends FileView {
         await this.saveTimelog();
       }
     }
-  }
-
-  XX() {
-    return <div>XX</div>;
   }
 }
 
@@ -407,97 +394,97 @@ class PlanModal extends Modal {
 }
 
 /**
- * 整个视图内容，挂载到 obsidian view 的 content 上
- */
-function ViewX() {
-  return (
-    <>
-      <Ctl timeStr="timeStr"/>
-    </>
-  );
-}
-
-/**
- * 视图控制区，控制面板
- */
-function Ctl({ timeStr }: { timeStr: string }) {
-  return (
-    <div className="timelog-ctl">
-      <Clock timeStr={timeStr} />
-    </div>
-  );
-}
-
-/**
- * 视图内容区
- */
-function Ctx({plans}: {plans: Plan[]}) {
-  return (<>
-    <PlansTable plans={plans}/>
-  </>);
-}
-
-/**
  * 刷新计划视图（表格视图）
  */
-function PlansTable({ plans }: { plans: Plan[] }) {
-  return (
-    <>
-      <h2>计划</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>名称</th>
-          </tr>
-        </thead>
-        <tbody>
-          {plans.map(plan => (
-            <tr key={plan.id}>
-              <td>{plan.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+function PlansTable(mountedEl: HTMLElement): (plans: Plan[]) => void {
+  const rootEl = mountedEl.createDiv({});
+  rootEl.innerHTML = /*html*/ `
+    <h2>计划</h2>
+    <table>
+      <thead><tr><th>名称</th></tr></thead>
+      <tbody></tbody>
+    </table>
+  `;
+  /**
+   * 渲染视图
+   */
+  const bodyEl = rootEl.querySelector("table tbody");
+  return plans => {
+    bodyEl?.empty();
+    let body = "";
+    for (const plan of plans) {
+      body += /*html*/ `<tr><td>${plan.name}</td></tr>`;
+    }
+    if (bodyEl) bodyEl.innerHTML = body;
+  };
 }
 
 /**
  * 刷新记录视图（表格形式）
  */
-function RecordsTable({ plans, records }: { plans: Plan[]; records: Record[] }) {
-  return (
-    <>
-      <h2>记录</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>计划</th>
-            <th>时长</th>
-            <th>开始时间</th>
-            <th>结束时间</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records.map(record => (
-            <tr key={record.start + record.stop}>
-              <td>{plans.find(plan => record.id === plan.id)!.name ?? ""}</td>
-              <td>{timeSub(record.start, record.stop, TIME_FMT)}</td>
-              <td>{record.start}</td>
-              <td>{record.stop}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+function RecordsTable(mountedEl: HTMLElement): (plans: Plan[], records: Record[]) => void {
+  /**
+   * HTML
+   */
+  const rootEl = mountedEl.createDiv({});
+  rootEl.innerHTML = /*html*/ `
+    <h2>记录</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>计划</th>
+          <th>时长</th>
+          <th>开始时间</th>
+          <th>结束时间</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  `;
+  /**
+   * 视图刷新
+   */
+  const tbodyEl: HTMLTableSectionElement = rootEl.querySelector("table tbody")!;
+  return (plans, records) => {
+    tbodyEl.empty();
+    let body = "";
+    for (const record of records) {
+      body += /*html*/ `
+        <tr>
+          <td>${plans.find(plan => record.id === plan.id)!.name ?? ""}</td>
+          <td>${timeSub(record.start, record.stop, TIME_FMT)}</td>
+          <td>${record.start}</td>
+          <td>${record.stop}</td>
+        </tr>
+      `;
+    }
+    tbodyEl.innerHTML = body;
+  };
 }
 
-export function Clock({ timeStr }: { timeStr: string }) {
-  const container: CSS.Properties = {
-    // padding: "2em",
-  };
-  const wrapper: CSS.Properties = {
+/**
+ * 时钟组件
+ * @param mountedEl 挂载点
+ * @returns render 刷新视图的回调函数
+ */
+function Clock(mountedEl: HTMLElement): (dateStr: string, timeStr: string) => void {
+  /**
+   * HTML
+   */
+  const rootEl = mountedEl.createDiv({ cls: "tl-container" });
+  rootEl.innerHTML = /*html*/ `
+    <div class="tl-wrapper">
+      <div class="text">YYYY-MM-DD</div>
+      <div class="text">HH:mm:ss</div>
+    </div>
+  `;
+  /**
+   * CSS
+   */
+  rootEl?.setCssStyles({});
+  const wrapperEl: HTMLDivElement | null = rootEl.querySelector(".tl-wrapper");
+  wrapperEl?.setCssStyles({
     display: "flex",
     alignSelf: "center",
     justifyContent: "space-around",
@@ -505,20 +492,123 @@ export function Clock({ timeStr }: { timeStr: string }) {
     padding: "1em",
     gap: "1em",
     border: "var(--hr-color) solid 3px",
-    borderRadius: "2em",
-  };
-  const text: CSS.Properties = {
-    color: "var(--h2-color)",
-    fontSize: "var(--h2-size)",
-    fontWeight: "bold",
-    lineHeight: "var(--h2-line-height)",
-  };
-  return (
-    <div style={container}>
-      <div style={wrapper}>
-        <div style={text}>{timeStr.replace(/\s[0-9:]*$/, "")}</div>
-        <div style={text}>{timeStr.replace(/^[0-9\-]*\s/, "")}</div>
-      </div>
-    </div>
+    borderRadius: "1em",
+  });
+  const textEls: NodeListOf<HTMLDivElement> = rootEl.querySelectorAll(".text");
+  textEls.forEach(textEl =>
+    textEl.setCssStyles({
+      color: "var(--h2-color)",
+      fontSize: "var(--h2-size)",
+      fontWeight: "bold",
+      lineHeight: "var(--h2-line-height)",
+    })
   );
+  /**
+   * 视图刷新
+   */
+  const dateEl = textEls[0];
+  const timeEl = textEls[1];
+  return (dateStr, timeStr) => {
+    dateEl.innerText = dateStr;
+    timeEl.innerText = timeStr;
+  };
+}
+
+/**
+ * 计划选择器
+ */
+function PlanSelect(mountedEl: HTMLElement, onSelectChange: (evt: Event) => void): (plans: Plan[], records: Record[]) => string {
+  /**
+   * HTML
+   */
+  const rootEl = mountedEl.createDiv();
+  rootEl.innerHTML = /*html*/ `
+    <select class="tl-dropdown">
+      <option><option>
+    </select>
+  `;
+  /**
+   * CSS
+   */
+  rootEl.setCssStyles({
+    display: "flex",
+    flexWrap: "wrap",
+  });
+  const dropdownEl = rootEl.querySelector(".tl-drow");
+  /**
+   * render
+   */
+  const selectEl = rootEl.querySelector("select")!;
+  selectEl.addEventListener("change", onSelectChange);
+  return (plans, records) => {
+    selectEl.empty();
+    let options = "";
+    for (const plan of plans) {
+      options += /*html*/ `
+        <option value="${plan.id}">${plan.name}</option>
+      `;
+    }
+    selectEl.innerHTML = options;
+    if (records.length !== 0) selectEl.value = records.last()!.id;
+    return selectEl.value;
+  };
+}
+
+/**
+ * 开始按钮
+ */
+class StartButton {
+  mountedEl: HTMLElement;
+  rootEl: HTMLElement;
+  wrapperEl: HTMLElement;
+  btnCpt;
+  constructor(mountedEl: HTMLElement){
+    this.mountedEl = mountedEl;
+    this.rootEl = this.mountedEl.createDiv();
+    this.wrapperEl = this.rootEl.createDiv();
+    this.btnCpt = new ExtraButtonComponent(this.wrapperEl);
+    this.initCss();
+  }
+  initCss() {
+    this.rootEl.setCssStyles({
+      padding: '1em',
+      border: "var(--hr-color) solid 3px",
+      borderRadius: '1em'
+    });
+    this.wrapperEl.setCssStyles({
+    })
+  }
+  onClick(cb: () => any): this {
+    this.btnCpt.onClick(cb);
+    return this;
+  }
+  toggleIcon(status: boolean) {
+    if (status) {
+      this.btnCpt.setIcon('square');
+    } else {
+      this.btnCpt.setIcon('play');
+    }
+  }
+}
+
+class Ctl3 {
+  mountedEl: HTMLElement;
+  rootEl: HTMLElement;
+  constructor(mountedEl: HTMLElement) {
+    this.rootEl = createDiv();
+    //
+
+    //
+    this.initCss();
+    if (mountedEl) {
+      mountedEl.appendChild(this.rootEl);
+      this.mountedEl = mountedEl;
+    }
+  }
+  private initCss() {
+    this.rootEl.setCssStyles({
+      display: "flex",
+      flexWrap: "wrap",
+    });
+  }
 }
